e8dff372dbbe2f75fafa6b08daf05d266474e5e3
Laian <braumlaian@gmail.com>
2024-07-31 22:59:37 -0300

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "royal-purple": "#8E6DE5",
+      "royal-purple-darker": "#9066FF",
+      "yellow-main": "#ffd666",
+    },
+  },
+  plugins: [],
+};
