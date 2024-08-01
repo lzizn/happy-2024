1cd20207f168a7e95f14e34de4d099697b3a3401
Laian <braumlaian@gmail.com>
2024-08-01 01:28:00 -0300

    feat: add OrphanagePoint and Orphanage types

---
+++ src/types/orphanage.ts
@@ -0,0 +1,22 @@
+export interface OrphanagePoint {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
+export interface Orphanage {
+  id: string;
+  name: string;
+  description: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: string;
+  latitude: number;
+  longitude: number;
+  images: {
+    name: string;
+    path: string;
+    url: string;
+  }[];
+}
